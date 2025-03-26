ee1110fea2f33072afebada061c2dab88dbf2f17
Prateek <prateekpm123@gmail.com>
2025-03-27 00:47:09 +0530

    Corrected the UI for Excercise Details page.

--- src/components/ExcerciseDetail.tsx
+++ src/components/ExcerciseDetail.tsx
@@ -1,4 +1,11 @@
-import { Button, Dialog, Flex, Heading, Text } from "@radix-ui/themes";
+import {
+  Button,
+  Dialog,
+  Flex,
+  Heading,
+  ScrollArea,
+  Text,
+} from "@radix-ui/themes";
 import { iExcerciseData } from "../models/ExcerciseInterface";
 import React from "react";
 
@@ -19,47 +26,58 @@ export const ExcerciseDetail = (excerciseDetailProp: iExcerciseDetailProps) => {
         <Dialog.Title>
           {excerciseDetailProp.excercise?.excercise_name}
         </Dialog.Title>
-        <Flex direction="row" height="100%" width="100%" gap="6" align="baseline" justify="start">
-          <Flex direction="row" justify="between" minWidth="300px">
+        <ScrollArea style={{ height: "40rem" }}>
+          <Flex
+            direction="row"
+            height="100%"
+            width="100%"
+            gap="6"
+            align="start"
+            justify="start"
+          >
+            {/* <Flex direction="row" justify="between" minWidth="300px"> */}
             <img
               src={excerciseDetailProp.excercise.excercise_image_url}
               alt={excerciseDetailProp.excercise.excercise_name}
-              style={{ height: "50%", width: "50%" }}
+              style={{ height: "50%", width: "50%", maxWidth: "25rem" }}
             />
-          </Flex>
-          <Flex direction="column" gap="4">
-            <Heading size="4" >
-              Description
-            </Heading>
-            {/* <Heading size="5">
+            {/* </Flex> */}
+
+            <Flex direction="column" gap="4" minWidth="20rem">
+              <Heading size="4">Description</Heading>
+              {/* <Heading size="5">
               {excerciseDetailProp.excercise.excercise_description}
             </Heading> */}
 
-            <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
-              {excerciseDetailProp.excercise.excercise_description
-                .split("\n")
-                .map((point: string, index: number) => (
-                  <li key={index}>{point}</li>
-                ))}
-            </ul>
+              <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
+                {excerciseDetailProp.excercise.excercise_description
+                  .split("\n")
+                  .map((point: string, index: number) => (
+                    <li key={index}>{point}</li>
+                  ))}
+              </ul>
+            </Flex>
+            <Flex direction="column" style={{ minWidth: "20rem" }}>
+              <Text weight="bold">Set and Repetition</Text>
+              <Text color="gray">
+                {excerciseDetailProp.excercise.excercise_sets} {" - "}
+                {excerciseDetailProp.excercise.excercise_sets_description}
+              </Text>
+              <Text color="gray">
+                {excerciseDetailProp.excercise.excercise_reps} {" - "}
+                {excerciseDetailProp.excercise.excercise_reps_description}
+              </Text>
+            </Flex>
           </Flex>
-          <Flex direction="column">
-            <Text weight="bold">
-              Set and Repetition
-            </Text>
-            <Text color="gray">
-              {excerciseDetailProp.excercise.excercise_sets} {" - "}
-              {excerciseDetailProp.excercise.excercise_sets_description}
-            </Text>
-            <Text color="gray">
-              {excerciseDetailProp.excercise.excercise_reps} {" - "}
-              {excerciseDetailProp.excercise.excercise_reps_description}
-            </Text>
-          </Flex>
-        </Flex>
-        <Flex gap="3" mt="4" justify="end" style={{position: "absolute", bottom: "1rem", right: "1rem"}}>
+        </ScrollArea>
+        <Flex
+          gap="3"
+          mt="4"
+          justify="end"
+          style={{ position: "absolute", bottom: "1rem", right: "1rem" }}
+        >
           <Dialog.Close>
-            <Button variant="soft" color="gray">
+            <Button variant="solid" color="gray">
               Cancel
             </Button>
           </Dialog.Close>
